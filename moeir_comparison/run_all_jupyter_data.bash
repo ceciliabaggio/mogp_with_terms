b64819e15ebff3860988421a9925b6e24d4f0cb5
Cecilia <ceciliabgg@gmail.com>
2023-09-25 15:51:59 -0300

    comparison finished. 3 plots

--- moeir_comparison/run_all_jupyter_data.bash
+++ moeir_comparison/run_all_jupyter_data.bash
@@ -13,3 +13,8 @@ jupyter nbconvert --execute --to notebook --inplace moeir_precision10.ipynb
 jupyter nbconvert --execute --to notebook --inplace moeir_globalRecall.ipynb
 jupyter nbconvert --execute --to notebook --inplace moeir_jaccard.ipynb
 
+# plots - correrlos aparte. falla el plot si se corre desde bash
+# jupyter nbconvert --execute --to notebook --inplace mogp_vs_moeir_p10.svg
+# jupyter nbconvert --execute --to notebook --inplace mogp_vs_moeir_gr.svg
+# jupyter nbconvert --execute --to notebook --inplace mogp_vs_moeir_jsi.svg
+
