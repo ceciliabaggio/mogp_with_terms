fe91d8cb8ff9ca9ba06bd3aae8a45cec0ac48a42
Cecilia <ceciliabgg@gmail.com>
2023-10-18 10:38:03 -0300

    dir cleaning

--- borrar.sh
+++
@@ -1,7 +0,0 @@
-./mogp_training.bash 2 150 10 1 69 0.7 0.3 /home/cecilia/repos/mogp_with_terms/runs/mogp_$(date +%F_%H-%M-%S) hadoop_pepe 1 0 
-./mogp_training.bash 2 150 10 2 69 0.7 0.3 /home/cecilia/repos/mogp_with_terms/runs/mogp_$(date +%F_%H-%M-%S) hadoop_pepe 1 0 
-./mogp_training.bash 2 150 10 3 69 0.7 0.3 /home/cecilia/repos/mogp_with_terms/runs/mogp_$(date +%F_%H-%M-%S) hadoop_pepe 1 0 
-./mogp_training.bash 2 150 10 4 69 0.7 0.3 /home/cecilia/repos/mogp_with_terms/runs/mogp_$(date +%F_%H-%M-%S) hadoop_pepe 1 0 
-./mogp_training.bash 2 150 10 5 69 0.7 0.3 /home/cecilia/repos/mogp_with_terms/runs/mogp_$(date +%F_%H-%M-%S) hadoop_pepe 1 0 
-./mogp_training.bash 2 150 10 6 69 0.7 0.3 /home/cecilia/repos/mogp_with_terms/runs/mogp_$(date +%F_%H-%M-%S) hadoop_pepe 1 0 
-./mogp_training.bash 2 150 10 7 69 0.7 0.3 /home/cecilia/repos/mogp_with_terms/runs/mogp_$(date +%F_%H-%M-%S) hadoop_pepe 1 0 
